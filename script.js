47d52a630ba38f698713338819771f41b03eeda2
Rafael Soares <fael3562@gmail.com>
2023-09-06 08:03:57 -0300

    Projeto em andamento

--- script.js
+++ script.js
@@ -2,4 +2,21 @@
     const currentOperationText = document.querySelector("#current-operation");
     const buttons = document.querySelectorAll("#buttons-container button");
 
-    
+    class Calculator {
+        constructor(previousOperationText, currentOperationText) {
+            this.previousOperationText = previousOperationText
+            this.currentOperationText = currentOperationText
+        }
+    }
+
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            const value = e.target.innerText;
+
+            if (+value >= 0 || value === ".") {
+                console.log(value);
+            } else {
+                console.log("Operação");
+            }
+        });
+    });
